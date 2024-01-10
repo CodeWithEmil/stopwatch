78d0e3d848e4d18af12aaecca6f5896eb5a50fe7
Emil <codewithemil@gmail.com>
2024-01-09 19:29:29 -0600

    Final version of this practice project. I really enjoyed being back and putting my knowledge to use again! For this last instance of the project, I updated the time variables into an object, and finished setting up the reset button, as well as adding functionality from the spacebar, enter and backspace keys (starting / stopping, and resetting the timer respectively).

--- scripts/index.js
+++ scripts/index.js
@@ -7,35 +7,61 @@ let stopEvent = document.querySelector(".js-stop-button").addEventListener("clic
    stopStopwatch();
 });
 
+const resetEvent = document.querySelector(".js-reset-button").addEventListener("click", () => {
+   resetStopwatch();
+})
+
+
+document.addEventListener("keydown", (event) => {
+   if (event.key === " " || event.key === "Enter") {
+      if (isStopwatchRunning === false) {
+         startStopwatch();
+      } else {
+         stopStopwatch();
+      }
+   } else if (event.key === "Backspace") {
+      resetStopwatch();
+   }
+})
+
 
 //* Global variables
 let isStopwatchRunning = false;
-
 let interval;
 
-let countHours = 0;
-let countMinutes = 0;
-let countSeconds = 0;
-let countMiliseconds = 0;
+let countTime = {
+   hours: 0,
+   minutes: 0,
+   seconds: 0,
+   miliseconds: 0
+}
+
+/*let countHourds = 0;
+let countMinuteds = 0;
+let countSecondds = 0;
+let countMilisecondds = 0;*/
 
 function startStopwatch() {
+   //Starting the stopwatch: if the milisecond time is greater than 999 (1 second), the milisecond time is reset and increases seconds by one. This same logic is applied from seconds to minutes and minutes to hours
+
    if (!isStopwatchRunning) {
       interval = setInterval(() => {
-         if (countMiliseconds <= 999) {
-            countMiliseconds+=Math.round(4.1);
+         if (countTime.miliseconds <= 998) {
+            countTime.miliseconds+=Math.round(4.1);
+            //countTime.miliseconds++;
          } else {
-            countMiliseconds = 0;
+            countTime.miliseconds = 0;
 
-            if (countSeconds < 59) {
-               countSeconds++;
+            if (countTime.seconds < 59) {
+               countTime.seconds++;
             } else {
-               countSeconds = 0;
+               countTime.seconds = 0;
 
-               if (countMinutes < 59) {
-                  countMinutes++;
+               if (countTime.minutes < 59) {
+                  countTime.minutes++;
                } else {
-                  countMinutes = 0;
-                  countHours++;
+                  countTime.minutes = 0;
+                  countTime.hours++;
                }
             }
          }
@@ -50,6 +76,8 @@ function startStopwatch() {
 }
 
 function operatingText() {
+   //Function declared to manage the text and the way it is displayed: basically adding 0's in front of the seconds display case they're needed (when the value is less than 10)
+
    const timeEl = document.querySelector(".js-stopwatch-seconds");
    const milisecondsEl = document.querySelector(".js-stopwatch-miliseconds");
 
@@ -58,30 +86,30 @@ function operatingText() {
    let messageSeconds;
    let messageMiliseconds;
 
-   if (countHours <= 9) {
-      messageHours = `0${countHours}`;
+   if (countTime.hours <= 9) {
+      messageHours = `0${countTime.hours}`;
    } else {
-      messageHours = countHours;
+      messageHours = countTime.hours;
    }
 
-   if (countMinutes <= 9) {
-      messageMinutes = `0${countMinutes}`;
+   if (countTime.minutes <= 9) {
+      messageMinutes = `0${countTime.minutes}`;
    } else {
-      messageMinutes = countMinutes;
+      messageMinutes = countTime.minutes;
    }
 
-   if (countSeconds <= 9) {
-      messageSeconds = `0${countSeconds}`;
+   if (countTime.seconds <= 9) {
+      messageSeconds = `0${countTime.seconds}`;
    } else {
-      messageSeconds = countSeconds;
+      messageSeconds = countTime.seconds;
    }
 
-   if (countMiliseconds <= 9) {
-      messageMiliseconds = `00${countMiliseconds}`;
-   } else if (countMiliseconds <= 99) {
-      messageMiliseconds = `0${countMiliseconds}`;
+   if (countTime.miliseconds <= 9) {
+      messageMiliseconds = `00${countTime.miliseconds}`;
+   } else if (countTime.miliseconds <= 99) {
+      messageMiliseconds = `0${countTime.miliseconds}`;
    } else {
-      messageMiliseconds = countMiliseconds;
+      messageMiliseconds = countTime.miliseconds;
    }
 
    timeEl.innerHTML = `<p>${messageHours}:${messageMinutes}:${messageSeconds}</p>`;
@@ -89,6 +117,8 @@ function operatingText() {
 }
 
 function stopStopwatch() {
+   //Stopping the stopwatch
+
    if (isStopwatchRunning) {
       clearInterval(interval);
 
@@ -98,6 +128,34 @@ function stopStopwatch() {
 }
 
 function disableButton(classOne, classTwo) {
+   //Basically taking the pressed button and giving it the class "disabled" (makes button unclickable), and removing it from the other button
+
    document.querySelector(`.js-${classOne}-button`).classList.add("disabled");
    document.querySelector(`.js-${classTwo}-button`).classList.remove("disabled");
+}
+
+function resetStopwatch() {
+   //Function resetting the stopwatch and all the adjount values from the countTime object, ass well as intervals and CSS classess
+
+   clearInterval(interval);
+   interval = null;
+
+   isStopwatchRunning = false;
+
+   countTime = { ...countTime, hours: 0, minutes: 0, seconds: 0, miliseconds: 0 };
+
+   /*countTime.hours = 0;
+   countTime.minutes = 0;
+   countTime.seconds = 0;
+   countTime.miliseconds = 0;*/
+
+   document.querySelector(".js-start-button").classList.remove("disabled");
+   document.querySelector(".js-stop-button").classList.remove("disabled");
+
+   document.querySelector(".js-reset-button").classList.add("reset-button-active");
+   setTimeout(() => {
+      document.querySelector(".js-reset-button").classList.remove("reset-button-active");
+   }, 200);
+
+   operatingText();
 }
